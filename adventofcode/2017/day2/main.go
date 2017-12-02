a6605c8e98e0be827ceaa9bd4f04ee2043da7088
Guillaume Rose <guillaume.rose@docker.com>
2017-12-02 09:10:08 +0100

    day2 part2

--- adventofcode/2017/day2/main.go
+++ adventofcode/2017/day2/main.go
@@ -6,25 +6,37 @@ import (
 	"os"
 	"bufio"
 	"fmt"
-	"math"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	sum := 0
 	for scanner.Scan() {
-		min := math.MaxInt32
-		max := math.MinInt32
-		for _, i := range strings.Fields(scanner.Text()) {
-			j, _ := strconv.Atoi(i)
-			if min > j {
-				min = j
-			}
-			if max < j {
-				max = j
+		line := scanner.Text()
+		for _, a := range strings.Fields(line) {
+			i, _ := strconv.Atoi(a)
+			divide := divide(line, i)
+			if divide != 0 {
+				sum += divide
+				break
 			}
 		}
-		sum += max - min
 	}
 	fmt.Println(sum)
+}
+
+func divide(line string, i int) int {
+	for _, b := range strings.Fields(line) {
+		j, _ := strconv.Atoi(b)
+		if i == j {
+			continue
+		}
+		if j%i == 0 {
+			return j / i
+		}
+		if i%j == 0 {
+			return i / j
+		}
+	}
+	return 0
 }
