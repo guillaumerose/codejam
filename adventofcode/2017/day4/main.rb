9aab95c382b1d22cfbae00bd8df4ff5a04a5236f
Guillaume Rose <guillaume.rose@docker.com>
2017-12-04 09:29:53 +0100

    day2 part2

--- adventofcode/2017/day4/main.rb
+++ adventofcode/2017/day4/main.rb
@@ -1,8 +1,16 @@
+require 'set'
+
 i = 0
 while str = gets
     words = str.strip.split(" ")
     if words.uniq.size == words.size
-        i = i + 1
+        seen = {}
+        words.each do |w|
+            seen[Set.new(w.split(''))] = true
+        end
+        if seen.size == words.size
+            i = i + 1
+        end
     end
 end
 puts i
