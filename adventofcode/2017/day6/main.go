2d641c3afeeb9e000cc53b8da512f6008077ac85
Guillaume Rose <guillaume.rose@docker.com>
2017-12-06 11:16:12 +0100

    day6 part1

---
+++ adventofcode/2017/day6/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	seen := make(map[string]bool)
+	input := []int{4, 10, 4, 1, 8, 4, 9, 14, 5, 1, 14, 15, 0, 15, 3, 5}
+
+	i := 0
+	for {
+		cycle(input)
+		i++
+		if _, ok := seen[hash(input)]; ok {
+			break
+		}
+		seen[hash(input)] = true
+	}
+	fmt.Println(i)
+}
+
+func hash(input []int) string {
+	return fmt.Sprintf("%v", input)
+}
+
+func cycle(input []int) {
+	val, index := max(input)
+	input[index] = 0
+	for j := 1; j <= val; j++ {
+		input[(index+j)%len(input)] += 1
+	}
+}
+
+func max(input []int) (int, int) {
+	index := -1
+	max := -1
+	for i := 0; i < len(input); i++ {
+		if max < input[i] {
+			max = input[i]
+			index = i
+		}
+	}
+	return max, index
+}
