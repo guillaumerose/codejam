dc87e6238bb39d4e9f0c81bf52b01ad0a0e87763
Guillaume Rose <guillaume.rose@docker.com>
2017-12-08 10:55:33 +0100

    day8 part2

--- adventofcode/2017/day8/translate.sh
+++ adventofcode/2017/day8/translate.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-cat $1 | cut -d' ' -f1 | sed 's/$/ = 0/'
-cat $1 | sed 's/inc/+=/' | sed 's/dec/-=/'
 FOO=$(cat $1 | cut -d' ' -f1 | sed 's/$/,/' | tr -d '\n')
-echo \[$FOO\].max
+echo max = 0
+cat $1 | cut -d' ' -f1 | sed 's/$/ = 0/'
+cat $1 | sed 's/inc/+=/' | sed 's/dec/-=/' | sed s/$/\;max=\[max,$FOO\].max/
+echo max
