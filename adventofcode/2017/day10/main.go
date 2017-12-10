93d797e20c4a051692768785a7f5e63c18da56d8
Guillaume Rose <guillaume.rose@docker.com>
2017-12-10 09:20:59 +0100

    day10 part2

--- adventofcode/2017/day10/main.go
+++ adventofcode/2017/day10/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"encoding/hex"
+)
+
 func main() {
 	input := make([]int, 256)
 	for i := 0; i < 256; i++ {
@@ -7,6 +11,43 @@ func main() {
 	}
 	lengths := []int{106, 118, 236, 1, 130, 0, 235, 254, 59, 205, 2, 87, 129, 25, 255, 118}
 	reverse(lengths, input)
+}
+
+func hash(str string) string {
+	input := make([]int, 256)
+	for i := 0; i < 256; i++ {
+		input[i] = i
+	}
+	lengths := []int{49, 44, 50, 44, 51, 17, 31, 73, 47, 23}
+	if str == "" {
+		lengths = []int{17, 31, 73, 47, 23}
+	}
+	len := len(input)
+	skip := 0
+	current := 0
+	for r := 0; r < 64; r++ {
+		for _, length := range lengths {
+			for i := 0; i < length/2; i++ {
+				x := (current + i) % len
+				y := (current + length - 1 - i) % len
+				a := input[x]
+				b := input[y]
+				input[x] = b
+				input[y] = a
+			}
+			current = (current + length + skip) % len
+			skip++
+		}
+	}
+	acc := ""
+	for z := 0; z < 16; z++ {
+		xor := input[16*z]
+		for k := 16*z + 1; k < 16*(z+1); k++ {
+			xor ^= input[k]
+		}
+		acc += hex.EncodeToString([]byte{byte(xor)})
+	}
+	return acc
 }
 
 func reverse(lengths []int, input []int) {
--- adventofcode/2017/day10/main_test.go
+++ adventofcode/2017/day10/main_test.go
@@ -5,8 +5,13 @@ import (
 	"github.com/magiconair/properties/assert"
 )
 
-func TestFilter(t *testing.T) {
+func TestReverse(t *testing.T) {
 	input := []int{0, 1, 2, 3, 4}
 	reverse([]int{3, 4, 1, 5}, input)
 	assert.Equal(t, []int{3, 4, 2, 1, 0}, input)
+}
+
+func TestHash(t *testing.T) {
+	assert.Equal(t, "3efbe78a8d82f29979031a4aa0b16a9d", hash("1,2,3"))
+	assert.Equal(t, "a2582a3a0e66e6e86e3812dcb672a272", hash(""))
 }
