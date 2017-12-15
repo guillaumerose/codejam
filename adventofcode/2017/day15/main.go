43728f6d9045ae4ce69733f46dc8cdd6893b5736
Guillaume Rose <guillaume.rose@docker.com>
2017-12-15 12:40:36 +0100

    day15 part2

--- adventofcode/2017/day15/main.go
+++ adventofcode/2017/day15/main.go
@@ -2,17 +2,24 @@ package main
 
 import "fmt"
 
-const pairs = 40000000
+const pairs = 5000000
 const factorA = 16807
 const factorB = 48271
+const mod = 2147483647
 
 func main() {
 	seedA := 873
 	seedB := 583
 	count := 0
 	for i := 0; i < pairs; i++ {
-		seedA = (seedA * factorA) % 2147483647
-		seedB = (seedB * factorB) % 2147483647
+		seedA = (seedA * factorA) % mod
+		for seedA%4 != 0 {
+			seedA = (seedA * factorA) % mod
+		}
+		seedB = (seedB * factorB) % mod
+		for seedB%8 != 0 {
+			seedB = (seedB * factorB) % mod
+		}
 
 		if seedA&0xFFFF == seedB&0xFFFF {
 			count++
